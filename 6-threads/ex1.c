a699f0c2b63766cc93a29ca070e2faa3841a2d5c
pepin2pomme <milosorel7@gmail.com>
2025-10-14 10:37:21 +0200

    feat(c): ajout fichier

---
+++ 6-threads/ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>  
+#include <sys/shm.h>  
+#include <sys/types.h>  
+#include <time.h>
+#include <ctype.h>
+#include <sys/sem.h>
+#include <errno.h>
+#include <pthread.h>
+
+void ThreadCalculus(){
+    
+}
