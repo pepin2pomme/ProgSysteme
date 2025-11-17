611ac3a75607037c73b1ebe216b6c15630672098
milo <milosorel7@gmail.com>
2025-11-17 11:30:01 +0100

    client envoie mesage au serv ok

--- 8-socket/ClientTCP.c
+++ 8-socket/ClientTCP.c
@@ -7,30 +7,59 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define PORT 12345
+int main(int argc, char *argv[]) {
 
-int main(int argc, char *argv[]){
-
-    if(argc!=3){
-        printf("Il faut 1 seul arguments\n");
+    if (argc != 4) {
+        printf("Usage: %s <adresse_IP> <port> <message>\n", argv[0]);
         return 1;
     }
 
     char *ipServeur = argv[1];
-    char *messageEnvoyer = argv[2];
-    
-    int descripteurSocket = socket(PF_INET, SOCK_STREAM, 0);
+    int port = atoi(argv[2]);
+    char *messageEnvoyer = argv[3];
+
+    int descripteurSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (descripteurSocket < 0) {
         perror("Erreur lors de la création de la socket");
-        exit(-1);
+        return 1;
     }
-    printf("Socket créée avec succès ! (%d)\n", descripteurSocket);
 
-    int connect(int sockfd, const struct sockaddr* serv_addr,socklen_t addrlen);
+    struct sockaddr_in adresseServeur;
+    memset(&adresseServeur, 0, sizeof(adresseServeur));
+    adresseServeur.sin_family = AF_INET;
+    adresseServeur.sin_port = htons(port);
 
+    if (inet_pton(AF_INET, ipServeur, &adresseServeur.sin_addr) <= 0) {
+        perror("Erreur de conversion d'adresse IP");
+        close(descripteurSocket);
+        return 1;
+    }
 
+    if (connect(descripteurSocket, (struct sockaddr *)&adresseServeur, sizeof(adresseServeur)) < 0) {
+        perror("Erreur de connexion");
+        close(descripteurSocket);
+        return 1;
+    }
 
-    
+    if (send(descripteurSocket, messageEnvoyer, strlen(messageEnvoyer), 0) < 0) {
+        perror("Erreur lors de l'envoi du message");
+        close(descripteurSocket);
+        return 1;
+    }
+
+    printf("Message envoyé au serveur: %s\n", messageEnvoyer);
+
+    char buffer[1024];
+    int bytesReçus = recv(descripteurSocket, buffer, sizeof(buffer) - 1, 0);
+    if (bytesReçus < 0) {
+        perror("Erreur de réception du message");
+        close(descripteurSocket);
+        return 1;
+    }
+
+    buffer[bytesReçus] = '\0';
+    printf("Réponse du serveur: %s\n", buffer);
+
     close(descripteurSocket);
 
     return 0;
